af318b18ebb8a4afaf88e3e10868dee9f3d3a868
Henrique Vianna <hvianna@gmail.com>
2021-03-04 19:05:13 -0300

    hotwheels: Add wishlist gallery 😁

--- hotwheels/index.js
+++ hotwheels/index.js
@@ -7,17 +7,19 @@
 import * as Papa from 'https://cdn.skypack.dev/papaparse';
 
 const sheetURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQA88VnFS7YDPOU9k8zT5Iax3Ju-6J3z4vkJhy9vdyw0Z1kK6j45I-r1THtmfy7_--LF3h4oYSrqk-l/pub?gid=0&single=true&output=csv';
+const wishesURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQA88VnFS7YDPOU9k8zT5Iax3Ju-6J3z4vkJhy9vdyw0Z1kK6j45I-r1THtmfy7_--LF3h4oYSrqk-l/pub?gid=1657118775&single=true&output=csv';
 
 // selector shorthand functions
 const $  = document.querySelector.bind( document );
 const $$ = document.querySelectorAll.bind( document );
 
 // globals
-const container = $('#container');
-const searchBox = $('#search');
-const modal     = $('#modal');
-const count     = $('#count');
-const loading   = $('#loading');
+const collection = $('#collection');
+const wishlist   = $('#wishlist');
+const searchBox  = $('#search');
+const modal      = $('#modal');
+const count      = $('#count');
+const loading    = $('#loading');
 
 let series = [];
 let years  = [];
@@ -39,37 +41,47 @@ function doSearch( event ) {
 	if ( value === undefined )
 		[ value, field ] = [ field, value ];
 
+	// search only on the selected gallery - collection or wishlist
+	const target = collection.classList.contains('hide') ? wishlist : collection;
+
 	// iterate over gallery items and hide those that don't match the search string
-	$$('.item').forEach( item => {
+	// if search string is empty, "unhide" all items on both galleries
+	( value === '' ? document : target).querySelectorAll('.item').forEach( item => {
 		const text = ( field ? item.dataset[ field ] : item.innerText ).toLowerCase();
-		item.classList.toggle( 'hide', value.length > 1 && ! text.includes( value ) );
+		item.classList.toggle( 'hide', value.length && ! text.includes( value ) );
 	});
 
-	count.innerText = $$('.item:not(.hide)').length;
+	// show count for displayed items
+	count.innerText = target.querySelectorAll('.item:not(.hide)').length;
 }
 
 // Initialization
 
 (function () {
+
+	const itemTemplate = ( item ) => {
+		const photo = item.image_url || ( item.image_id ? `https://lh3.googleusercontent.com/pw/${item.image_id}=w1400` : '' );
+		return `
+			<div class="item" data-year="${ item.year }" data-series="${ item.series }" data-part="${ item.part_no }">
+				<div class="photo">
+					${ photo ? '<img src="' + photo + '" loading="lazy">' : '' }
+					<div class="number">${ item.year_no }</div>
+					<div class="part">${ item.part_no }</div>
+				</div>
+				<div class="title">${ item.model }</div>
+				<div class="info">${ item.year } ${ item.series } (${ item.series_no })</div>
+			</div>
+		`;
+	}
+
+	// load collection spreadsheet
 	Papa.parse( sheetURL, {
 		download: true,
 		header: true,
 		step: result => {
 			// add new item to the gallery
 			const item = result.data;
-			const photo = item.image_id ? `https://lh3.googleusercontent.com/pw/${item.image_id}=w1400` : '';
-
-			container.innerHTML += `
-				<div class="item" data-year="${ item.year }" data-series="${ item.series }" data-part="${ item.part_no }">
-					<div class="photo">
-						${ photo ? '<img src="' + photo + '" loading="lazy">' : '' }
-						<div class="number">${ item.year_no }</div>
-						<div class="part">${ item.part_no }</div>
-					</div>
-					<div class="title">${ item.model }</div>
-					<div class="info">${ item.year } ${ item.series } (${ item.series_no })</div>
-				</div>
-			`;
+			collection.innerHTML += itemTemplate( item );
 
 			// save different series and years for menu
 			if ( ! series.includes( item.series ) )
@@ -110,8 +122,29 @@ function doSearch( event ) {
 		}
 	});
 
+	// load wishlist spreadsheet
+	Papa.parse( wishesURL, {
+		download: true,
+		header: true,
+		complete: result => {
+			// populate the gallery
+			const data = result.data;
+			data.forEach( item => wishlist.innerHTML += itemTemplate( item ) );
+
+			// show/hide wishlist
+			const toggle = $('#wishlist_toggle');
+			toggle.addEventListener( 'click', () => {
+				const active = toggle.classList.toggle('active');
+				wishlist.classList.toggle( 'hide', ! active );
+				collection.classList.toggle( 'hide', active );
+				count.innerText = ( active ? wishlist : collection ).querySelectorAll('.item:not(.hide)').length;
+				$('#title').innerText = active ? 'Wishlist' : 'Collection';
+			});
+		}
+	});
+
 	// show/hide submenus on hover or click
-	$$('.menu > li').forEach( el => {
+	$$('.menu > li:not([id])').forEach( el => {
 		el.addEventListener( 'mouseover', () => el.classList.toggle( 'active', true ) );
 		el.addEventListener( 'mouseout', () => el.classList.toggle( 'active', false ) );
 		el.addEventListener( 'click', () => el.classList.toggle( 'active' ) );
